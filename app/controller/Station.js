8e43a67c17a99eed41ada6485479b64038da8577
Joe Robles <joe.robles.pdj@gmail.com>
2013-06-17 17:07:45 -0500

    starting a new station

--- app/controller/Station.js
+++ app/controller/Station.js
@@ -6,10 +6,11 @@ Ext.define('Panda.controller.Station', {
     stores: ['Stations'],    // creates getter named 'getStationsStore'    -> returns the Stations store instance
     refs: [{
         // A component query
-        selector: 'viewport > #west-region > stationslist',
+        selector: 'stationsList',
         ref: 'stationsList'
     }],
     init: function() {
+        // Listen for the select event on the NewStation combobox
         this.control({
             'stationslist': {
                 selectionchange: this.onStationSelect
@@ -19,9 +20,6 @@ Ext.define('Panda.controller.Station', {
             }
         });
     },
-    onStationSelect: function(selModel, selection) {
-        this.application.fireEvent('stationstart', selection[0]);
-    },
     onLaunch: function() {
         // Use the automatically generated getter to get the store
         var stationsStore = this.getStationsStore();        
@@ -30,8 +28,24 @@ Ext.define('Panda.controller.Station', {
             scope: this
         });
     },
+    onNewStationSelect: function(field, selection) {
+        var selected = selection[0],
+            store = this.getStationsStore(),
+            list = this.getStationsList();
+
+        if (selected && !store.getById(selected.get('id'))) {
+            // If the newly selected station does not exist in our station store we add it
+            store.add(selected);
+        }
+
+        // We select the station in the Station list
+        list.getSelectionModel().select(selected);
+    },
     onStationsLoad: function() {
         var stationsList = this.getStationsList();
         stationsList.getSelectionModel().select(0);
+    },
+    onStationSelect: function(selModel, selection) {
+        this.application.fireEvent('stationstart', selection[0]);
     },
 });
