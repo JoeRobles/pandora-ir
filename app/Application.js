90b800112cb582c8d443c90d3c9c8ac8ae5b8ad0
Joe Robles <joe.robles.pdj@gmail.com>
2013-06-17 13:09:44 -0500

    stores and models added to application

--- app/Application.js
+++ app/Application.js
@@ -1,6 +1,8 @@
 Ext.application({
     name: 'Panda',    
     autoCreateViewport: true,
+    models: ['Station', 'Song'],    
+    stores: ['Stations', 'RecentSongs', 'SearchResults'],
     launch: function() {
         // This is fired as soon as the page is ready
     }
