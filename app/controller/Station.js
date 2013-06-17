b01c4b41cee8b948f2c97c5525e21f6d7de6616f
Joe Robles <joe.robles.pdj@gmail.com>
2013-06-17 14:34:36 -0500

    getting references

--- app/controller/Station.js
+++ app/controller/Station.js
@@ -1,5 +1,9 @@
 Ext.define('Panda.controller.Station', {
     extend: 'Ext.app.Controller',
+    views: ['StationsList'], // creates getter named 'getStationsListView' -> returns reference to StationsList class
+    models: ['Station'],     // creates getter named 'getStationModel'     -> returns reference to Station model class
+    controllers: ['Song'],   // creates getter named 'getSongController'   -> returns the Song controller instance
+    stores: ['Stations'],    // creates getter named 'getStationsStore'    -> returns the Stations store instance
     init: function() {
         this.control({
             'stationslist': {
