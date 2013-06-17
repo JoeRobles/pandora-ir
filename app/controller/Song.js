9003dd133f9e0386747cbbe98a435093e4b7848d
Joe Robles <joe.robles.pdj@gmail.com>
2013-06-17 14:29:46 -0500

    setting up listeners

--- app/controller/Song.js
+++ app/controller/Song.js
@@ -1,7 +1,16 @@
 Ext.define('Panda.controller.Song', {
     extend: 'Ext.app.Controller',
     init: function() {
-        //...
+        this.control({
+            'recentlyplayedscroller': {
+                selectionchange: this.onSongSelect
+            }
+        });
+
+        this.application.on({
+            stationstart: this.onStationStart,
+            scope: this
+        });
     },
     //...
 });
