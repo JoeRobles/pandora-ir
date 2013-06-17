ce474d5a4c7003064e19741f8dc80534b1171eba
Joe Robles <joe.robles.pdj@gmail.com>
2013-06-17 14:06:33 -0500

    controllers initialized

--- app/Application.js
+++ app/Application.js
@@ -3,6 +3,7 @@ Ext.application({
     autoCreateViewport: true,
     models: ['Station', 'Song'],    
     stores: ['Stations', 'RecentSongs', 'SearchResults'],
+    controllers: ['Station', 'Song'],
     launch: function() {
         // This is fired as soon as the page is ready
     }
